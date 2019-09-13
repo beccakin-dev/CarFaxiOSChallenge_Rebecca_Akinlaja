69f73779d9400827f121c6f929c520c23bf6e2a1
Rebecca Akinlaja <rebeccakinlaja@gmail.com>
2019-09-12 21:25:54 -0400

    Initial Commit

---
+++ CarFaxHomeProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CarFaxHomeProject
+//
+//  Created by Becca Akinlaja on 9/12/19.
+//  Copyright © 2019 Becca Akinlaja. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
